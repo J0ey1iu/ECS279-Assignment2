f7f135fbe473cb28800bbb26f82e1946a55efad1
刘佳雨 <liujiayu@liujiayus-MacBook-Pro.local>
2020-03-09 16:45:31 -0700

    init

---
+++ Assets/Scripts/RobotAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAnimator : MonoBehaviour
+{
+
+    private Animator animator;
+    private bool isAnimating = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent("RobotAnimator") as Animator;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isAnimating) {
+            animator.SetBool("isAnimating", true);
+            isAnimating = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isAnimating) {
+            animator.SetBool("isAnimating", false);
+            isAnimating = false;
+        }
+    }
+}
